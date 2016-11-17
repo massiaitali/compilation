296cd8ffc529ed005f8997de6c7cd465fa12e992
massi <aitali.massinissa.polytech@gmail.com>
2016-11-17 11:11:40 +0100

    Ajout du generateur de code

--- analyse_syntaxique/analyse_syntaxique.php
+++ analyse_syntaxique/analyse_syntaxique.php
@@ -9,6 +9,14 @@ class Analyseur_Synthaxique
 	public $arbre = array(); // arbre type : array
 	public $tableau_token = array(); // tableau de token 
 
+	public function look(){
+		return $this->tableau_token[$this->position + 1];
+	}
+	public function next{
+		$this->position ++;
+		return $this->tableau_token[$this->position];
+	}
+
 	public function ajouter_tableau($tableau)
 	{
 		$this->tableau_token = $tableau;
@@ -34,33 +42,36 @@ class Analyseur_Synthaxique
 		else return false;
 	}
 	public function primitive(){
-		if ($this->retourner_type_token($this->tableau_token[$this->position])== 'ident') {
-			$this->ajouter_noeud($this->parent, 'ident',$this->tableau_token[$this->position]);
-			$this->position++;
+		if ($this->retourner_type_token($this->look())== 'ident') {
+			$this->ajouter_noeud($this->parent, 'ident',$this->next());
 		}
-		if ($this->tableau_token[$this->position] == 304) {
-			$this->position++;
+		if ($this->look() == 304) {
+			$this->next();
 			$this->expression();
-			if ($this->tableau_token[$this->position] != 305 || $this->tableau_token[($this->position)+1] == null) {
+			if ($this->look() != 305 || $this->look() == null) {
 				echo 'Erreur pas de ) ou mauvaise expression';
 			}
 			else {
-				$this->position++;
+				$this->next();
 				return false;
 			}
 		}
 	}
 	public function additive(){
-		if ($this->tableau_token[($this->position)+1]==313 || $this->tableau_token[($this->position)+1]==300) {
-			$this->position++;
+		if ($this->look() != null) {
+			$this->ajouter_noeud($this->parent, 'ident',$this->next());
+		}
+		if ($this->look()==313 || $this->look()==300) {
+			$this->next();
 			$this->additive;
+
 		}
 	}
 
 	public function multiplicateur(){
 		primitive();
-		if($this->retourner_type_token($this->tableau_token[$this->position]) == 302 || $this->retourner_type_token($this->tableau_token[$this->position]) == 303 || $this->retourner_type_token($this->tableau_token[$this->position]) == 304) {
-			$this->position++;
+		if($this->retourner_type_token($this->look()) == 302 || $this->retourner_type_token($this->look()) == 303 || $this->retourner_type_token($this->look()) == 304) {
+			$this->next();
 			$this->multiplicateur();
 		}
 	}
@@ -68,7 +79,7 @@ class Analyseur_Synthaxique
 
 	}
 }
-$token_tab = Array ( "for/3/Mots Cles" => 200 ,"(/5/Operateur" => 304 ,"x/7/Ident" => 23 ,"=/9/Operateur" => 312 ,"0/11/Entier" => 108 ,";/12/Operateur" => 314 ,"x/14/Ident" => 23 ,"<=/17/Operateur" => 310 ,"1/19/Entier" => 100 ,"0/20/Entier" => 108 ,";/22/Operateur" => 314 ,"x/24/Ident" => 23 ,"=/26/Operateur" => 312 ,"x/28/Ident" => 23 ,"+/30/Operateur" => 313 ,"1/32/Entier" => 100 ,")/34/Operateur" => 305 ,"y/36/Ident" => 24 ,"=/38/Operateur" => 312 ,"x/40/Ident" => 23 ,"+/42/Operateur" => 313 ,"9/44/Entier" => 107 ,";/46/Operateur" => 314 );
+$token_tab = Array ( "for/3/MotsCles" => 200 ,"(/5/Operateur" => 304 );
 $test = new Analyseur_Synthaxique;
 $test-> ajouter_tableau($token_tab);
 $test->primitive();
---
+++ generateur_code/generateur_code.php
@@ -0,0 +1,7 @@
+<?php 
+class generateur_code 
+{
+
+}
+
+?>
